8d1b034a0b44c12cd0c21ccdca6077bc07ac073b
Zachary Goodman <znote89@gmail.com>
2021-08-31 12:32:59 -0400

    commit tree 5fbb7a8c08625f1a7b5cf490a06816c89c7d9a81

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,14 +2,16 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
     author = Author.create(author_params)
-
-    render json: author, status: :created
+    if author.valid?
+      render json: author, status: :created
+    else
+      render json: { errors: author.errors }, status: :unprocessable_entity
+    end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,15 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
